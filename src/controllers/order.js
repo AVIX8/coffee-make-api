d82437b236cfc2bd2e0d6699e43047c1f273d0c6
AVIX <avix256@gmail.com>
2021-07-26 23:42:25 +0700

    работает оформление заказа

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -2,35 +2,73 @@ const Product = require('../models/Product.js')
 
 const messages = require('../messages.js')
 
-async function getValidCart(cart) {
+async function getValidItems(items) {
+    if (!items?.length) throw messages.cartIsRequired
 
-    return new Promise((resolve, reject) => {
-        if (!cart?.length)
-            return reject(messages.cartIsRequired)
+    let totalPrice = 0
+    let validItems = await Promise.all(
+        items.map(async (x) => {
+            let product = await Product.findOne({
+                'variants.SKU': x.SKU,
+            })
+                .lean()
+                .catch((error) => {
+                    throw `Не корректный SKU (${JSON.stringify(error.value)})`
+                })
 
-        Promise.all(cart.map(async (x) => {
-            let product = await Product.findById(x._id)
-            if (x.optionValue == null) {
-                return product
-            }
-        })).then((products) => {
-            console.log(products);
-        }).catch(() => {
-            return reject(messages.invalidProduct)
+            if (product == null) throw `Товар с SKU ${x.SKU} не найден`
+
+            product.variants = [
+                product.variants.find((variant) => variant.SKU === x.SKU),
+            ]
+
+            if (!product.variants[0].inStock)
+                throw `Товара ${product.title} нет в наличии`
+
+            totalPrice += product.variants[0].price * x.quantity
+            return product
         })
-    });
+    )
+    return { items:validItems, totalPrice }
 }
 
-module.exports.get = async (req, res) => {}
+module.exports.get = async (req, res) => {
+    res.send(req.body)
+}
 
-module.exports.create = async (req, res) => {
-    console.log(req.body)
+module.exports.getValid = async (req, res) => {
+    let validItems
     try {
-        await getValidCart(req.body.cart)
+        validItems = await getValidItems(req.body.items)
     } catch (error) {
         res.status(400).send({ message: error })
     }
-    res.status(200).send()
+
+    res.status(200).send({validItems})
 }
 
-module.exports.createWithLogin = async (req, res) => {}
+module.exports.create = async (req, res) => {
+    console.log(req.body)
+    let validItems
+    try {
+        validItems = await getValidItems(req.body.items)
+    } catch (error) {
+        res.status(400).send({ message: error })
+    }
+    // console.log(JSON.stringify(validItems), JSON.stringify(req.validItems));
+    if (JSON.stringify(validItems) != JSON.stringify(req.body.validItems)) {
+        res.status(426).send({
+            message: 'Информация о товарах была обновлена',
+            validItems,
+        })
+    } else {
+        res.status(200).send({
+            message: 'Заказ успешно оформлен',
+            orderId: 'пока не работает',
+        })
+    }
+}
+
+module.exports.createWithLogin = async (req, res) => {
+    res.send(req.body)
+}
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,12 +1,13 @@
 const { Router } = require( 'express')
 const { isAuth } = require('../middlewares/auth')
 
-const { get, create, createWithLogin } = require('../controllers/order')
+const { get, getValid, create, createWithLogin } = require('../controllers/order')
 
 const router = Router()
 
 
 router.post('/getInfo', get)
+router.post('/getValid', getValid)
 
 router.post('/create', create)
 router.post('/createWithLogin', isAuth, createWithLogin)
